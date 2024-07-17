2d0f1a43b8aaf2af3b938b74e14200c033cb1270
JohnPaulPrabhu <jpprabhu33@gmail.com>
2024-07-17 17:11:06 +0530

    Adding the data loading and transformation script

---
+++ data_loading.py
@@ -0,0 +1,30 @@
+import os
+import torch
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader, random_split
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+save_path = 'path_to_save_models'
+os.makedirs(save_path, exist_ok=True)
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+dataset_folder = 'path_to_image_folder'
+dataset = ImageFolder(dataset_folder, transform=transform)
+
+train_size = int(0.7 * len(dataset))
+val_size = int(0.15 * len(dataset))
+test_size = len(dataset) - train_size - val_size
+train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
